d34ccd2c62925a961fc70db5d99f27a3575cce27
Adam Tupper <adam.tupper@outlook.com>
2019-05-03 18:46:36 +1200

    Add ANOVA and pairwised t-tests for speed analysis.

---
+++ src/speed_analysis.r
@@ -0,0 +1,35 @@
+# Perform a multifactor ANOVA  and pairwise t-test analysis for speed.
+#
+# User must provide the path to the TEMA log directory.
+#
+# Author: Adam Tupper
+# Since: 03/05/19
+
+#!/usr/bin/env Rscript
+
+library(ez)
+library(plyr)
+
+source("src/TEMA_log_parsing.r")
+
+# Check for log directory in command line arguments
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) == 0) {
+  stop("The path to the TEMA log directory must be supplied.", call.=FALSE)
+} else {
+  log_dir = args[1]
+}
+
+# Parse and combine log files into a dataframe
+log_data = combine_TEMA_stats_logs(log_dir)
+
+# Remove trials 1-5 for each technique and participant and calculate means
+log_data = log_data[!(log_data$trial %in% c(1, 2, 3, 4, 5)),]
+summarised_data = ddply(log_data, c("participant_id", "interface", "posture"), summarise, mean_wpm=mean(wpm))
+
+# Multifactor ANOVA
+ezANOVA(data=summarised_data, dv=mean_wpm, within=.(interface, posture), wid=participant_id)
+
+# Pairwise t-tests
+pairwise.t.test(summarised_data$mean_wpm, summarised_data$interface, p.adj="bonf", paired=T)
+pairwise.t.test(summarised_data$mean_wpm, summarised_data$posture, p.adj="bonf", paired=T)
