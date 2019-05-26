df474a90a10813168902ece425dee639c20b0985
Adam Tupper <adam.tupper@outlook.com>
2019-05-26 21:04:09 +1200

    Generate plots for speed, error and NASA-TLX results.

--- src/plot_creation.r
+++ src/plot_creation.r
@@ -8,6 +8,7 @@
 library(plyr)
 library(ggplot2)
 library(vwr)
+library(gridExtra)
 
 source("src/TEMA_log_parsing.r")
 
@@ -42,21 +43,130 @@ condition_data = ddply(log_data,
                        sd_cer=sd(cer))
 
 ggplot(condition_data, aes(posture, mean_wpm, fill=interface)) +
+  theme_bw() +
   geom_col(position="dodge") +
   geom_errorbar(aes(ymin=mean_wpm - sd_wpm, ymax=mean_wpm + sd_wpm), width=0.2, position=position_dodge(0.9)) +
-  scale_fill_discrete(name="Interface",
-                      labels=c("SGK", "STK")) +
+  scale_fill_manual(name="Interface",
+                    labels=c("SGK", "STK"),
+                    values=c(cbp[3], cbp[7])) +
   labs(title="Entry Rates by Posture and Interface", x="Posture", y="Mean Entry Rate (WPM)") +
   theme(plot.title=element_text(hjust=0.5, vjust=0.5),
         legend.position = c(0.01, 0.99), 
         legend.justification = c(0, 1))
   
 ggplot(condition_data, aes(posture, mean_cer, fill=interface)) +
+  theme_bw() +
   geom_col(position="dodge") +
-  geom_errorbar(aes(ymin=max(mean_cer - sd_cer, 0), ymax=mean_cer + sd_cer), width=0.2, position=position_dodge(0.9)) +
-  scale_fill_discrete(name="Interface",
-                      labels=c("SGK", "STK")) +
+  geom_errorbar(aes(ymin=pmax(mean_cer - sd_cer, 0), ymax=mean_cer + sd_cer), width=0.2, position=position_dodge(0.9)) +
+  scale_fill_manual(name="Interface",
+                    labels=c("SGK", "STK"),
+                    values=c(cbp[3], cbp[7])) +
   labs(title="Error Rates by Posture and Interface", x="Posture", y="Mean Error Rate (CER)") +
   theme(plot.title=element_text(hjust=0.5, vjust=0.5),
         legend.position = c(0.01, 0.99), 
         legend.justification = c(0, 1))
+
+# Plot NASA-TLX Results
+cbp <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
+          "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+tlx_condition_data = ddply(tlx_data,
+                           c("interface"),
+                           summarise,
+                           mean_mental_demand=mean(mental_demand),
+                           sd_mental_demand=sd(mental_demand),
+                           mean_physical_demand=sd(physical_demand),
+                           sd_physical_demand=sd(physical_demand),
+                           mean_temporal_demand=mean(temporal_demand),
+                           sd_temporal_demand=sd(temporal_demand),
+                           mean_performance=mean(performance),
+                           sd_performance=sd(performance),
+                           mean_effort=mean(effort),
+                           sd_effort=sd(effort),
+                           mean_frustration=mean(frustration),
+                           sd_frustration=sd(frustration))
+
+plot_mental_demand = ggplot(tlx_condition_data) +
+  geom_col(aes(interface, mean_mental_demand), color=cbp[2], fill=cbp[2]) +
+  geom_errorbar(aes(x=interface,
+                    ymin=pmax(mean_mental_demand - sd_mental_demand, 0),
+                    ymax=(mean_mental_demand + sd_mental_demand)),
+                width=0.2) +
+  labs(title="Mental Demand", x="Condition", y="Mean Score (Lower is Better)") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5, vjust=0.5),
+        legend.position = c(0.01, 0.99), 
+        legend.justification = c(0, 1)) +
+  ylim(0, 20)
+
+plot_physical_demand = ggplot(tlx_condition_data) +
+  geom_col(aes(interface, mean_physical_demand), color=cbp[3], fill=cbp[3]) +
+  geom_errorbar(aes(x=interface,
+                    ymin=pmax(mean_physical_demand - sd_physical_demand, 0),
+                    ymax=(mean_physical_demand + sd_physical_demand)),
+                width=0.2) +
+  labs(title="Physical Demand", x="Condition", y="Mean Score (Lower is Better)") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5, vjust=0.5),
+        legend.position = c(0.01, 0.99), 
+        legend.justification = c(0, 1)) +
+  ylim(0, 20)
+
+plot_temporal_demand = ggplot(tlx_condition_data) +
+  geom_col(aes(interface, mean_temporal_demand), color=cbp[4], fill=cbp[4]) +
+  geom_errorbar(aes(x=interface,
+                    ymin=pmax(mean_temporal_demand - sd_temporal_demand, 0),
+                    ymax=(mean_temporal_demand + sd_temporal_demand)),
+                width=0.2) +
+  labs(title="Rushed/Hurried", x="Condition", y="Mean Score (Lower is Better)") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5, vjust=0.5),
+        legend.position = c(0.01, 0.99), 
+        legend.justification = c(0, 1)) +
+  ylim(0, 20)
+
+plot_performance = ggplot(tlx_condition_data) +
+  geom_col(aes(interface, mean_performance), color=cbp[6], fill=cbp[6]) +
+  geom_errorbar(aes(x=interface,
+                    ymin=pmax(mean_performance - sd_performance, 0),
+                    ymax=(mean_performance + sd_performance)),
+                width=0.2) +
+  labs(title="Perceived Performance", x="Condition", y="Mean Score (Lower is Better)") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5, vjust=0.5),
+        legend.position = c(0.01, 0.99), 
+        legend.justification = c(0, 1)) +
+  ylim(0, 20)
+
+plot_effort = ggplot(tlx_condition_data) +
+  geom_col(aes(interface, mean_effort), color=cbp[7], fill=cbp[7]) +
+  geom_errorbar(aes(x=interface,
+                    ymin=pmax(mean_effort - sd_effort, 0),
+                    ymax=pmin(mean_effort + sd_effort, 20)),
+                width=0.2) +
+  labs(title="Effort", x="Condition", y="Mean Score (Lower is Better)") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5, vjust=0.5),
+        legend.position = c(0.01, 0.99), 
+        legend.justification = c(0, 1)) +
+  ylim(0, 20)
+
+plot_frustration = ggplot(tlx_condition_data) +
+  geom_col(aes(interface, mean_frustration), color=cbp[8], fill=cbp[8]) +
+  geom_errorbar(aes(x=interface,
+                    ymin=pmax(mean_frustration - sd_frustration, 0),
+                    ymax=(mean_frustration + sd_frustration)),
+                width=0.2) +
+  labs(title="Frustration", x="Condition", y="Mean Score (Lower is Better)") +
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5, vjust=0.5),
+        legend.position = c(0.01, 0.99), 
+        legend.justification = c(0, 1)) +
+  ylim(0, 20)
+
+grid.arrange(plot_mental_demand,
+             plot_physical_demand,
+             plot_temporal_demand,
+             plot_performance,
+             plot_effort,
+             plot_frustration,
+             ncol=2)
